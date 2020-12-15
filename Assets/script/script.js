589283594df84e93f01bda5e7a674e05a27371f3
lisa-2 <lisa.sa.li@ieee.org>
2020-12-15 17:16:36 -0500

    added modal images

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -60,3 +60,22 @@ function goToPage(name) {
     window.location.href = `projects/${name}.html`;
 }
 
+function modalImage(img) {
+    let modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    let modalImg = document.getElementById("img01");
+    let captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+}
